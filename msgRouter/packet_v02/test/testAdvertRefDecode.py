c4e66b271e42347a0fbb56e1e7628e7e2c31e7a2
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2008-12-03 13:26:42 -0700

    Added packet_v02 tests
    --HG--
    branch : refactor

---
+++ msgRouter/packet_v02/test/testAdvertRefDecode.py
@@ -0,0 +1,141 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+##~ Copyright (C) 2002-2009  TechGame Networks, LLC.              ##
+##~                                                               ##
+##~ This library is free software; you can redistribute it        ##
+##~ and/or modify it under the terms of the BSD style License as  ##
+##~ found in the LICENSE file included with this distribution.    ##
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Imports 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import unittest
+
+from TG.blathernet.adverts import advertIdForNS
+from TG.blathernet.msgRouter import packet_v02 as packet
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Definitions 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class TestAdvertRefDecode(unittest.TestCase):
+    advertId = advertIdForNS('testAdvertRefs')
+    msgId = '5678'
+
+    advertIdRefs = [
+        "098f6bcd4621d373cade4e832627b4f6",
+        "b6f1208de7a06b650e0e502dec1a0421",
+        "88c9f875b7cb417e8a6ed65fc3596aeb",
+        "2f6fb24d7651899f53dc757f03ea4be9",
+        "311e00fbd7fecdbffeb3cab327864f34",
+        "812d363384d626e8c3610d46541eb0f6",
+        "c58205ce914f74f9a968f24fbd332cd2",
+        "7bf7250cd9586e095d2f03f044b4e8b6",
+        "b06f8815af69d60f7d919d5ed17ece61",
+        "58920777bc04ad5f4b9841503162af09",
+        "9b1eabbb244690821c58fd77fd0470c4",
+        "55a557ae0ca4848abdc406f5b927da2e",
+        "27dc9bc1544df3a03087a2a7776d0268",
+        "454116acd8ee9e968f643fe28f24f32b",
+        "b2a6c44eba3dbc99f4fb386c60b996c0",
+        "0197961ffce1c3b7a9ef7e50f447e27d",
+        ]
+    advertIdRefs = [e.decode('hex') for e in advertIdRefs]
+
+    def buildMsgObj(self, data, nCmds):
+        mobj = packet.MsgObject(data)
+        r = list(mobj.cmdList)
+        self.assertEqual(len(r),  nCmds)
+        return mobj, r
+
+    def testDecodeOneRef(self):
+        data = ("02353637387a6a431b669f1914f6313bc405905e1340098f6bcd4621d373cade4e832627b4f6").decode("hex")
+        mobj, r = self.buildMsgObj(data, 1)
+
+        adCmd, (adRefs, adRefKey) = r[0]
+        self.assertEqual(adCmd, "advertIdRefs")
+        self.assertEqual(len(adRefs), 1)
+        self.assertEqual(adRefs, self.advertIdRefs[:1])
+        self.assertEqual(adRefKey, None)
+
+
+    def testDecodeOneRefKey(self):
+        data = ("02353637387a6a431b669f1914f6313bc405905e135004616b6579098f6bcd4621d373cade4e832627b4f6").decode("hex")
+        mobj, r = self.buildMsgObj(data, 1)
+
+        adCmd, (adRefs, adRefKey) = r[0]
+        self.assertEqual(adCmd, "advertIdRefs")
+        self.assertEqual(len(adRefs), 1)
+        self.assertEqual(adRefs, self.advertIdRefs[:1])
+        self.assertEqual(adRefKey, 'akey')
+
+
+    def testDecode5Ref(self):
+        data = ("02353637387a6a431b669f1914f6313bc405905e1344098f6bcd4621d373cade4e832627b4f6b6f1208de7a06b650e0e502dec1a042188c9f875b7cb417e8a6e"
+            "d65fc3596aeb2f6fb24d7651899f53dc757f03ea4be9311e00fbd7fecdbffeb3cab327864f34").decode("hex")
+        mobj, r = self.buildMsgObj(data, 1)
+
+        adCmd, (adRefs, adRefKey) = r[0]
+        self.assertEqual(adCmd, "advertIdRefs")
+        self.assertEqual(len(adRefs), 5)
+        self.assertEqual(adRefs, self.advertIdRefs[:5])
+        self.assertEqual(adRefKey, None)
+
+
+    def testDecode5RefKey(self):
+        data = ("02353637387a6a431b669f1914f6313bc405905e13540466697665098f6bcd4621d373cade4e832627b4f6b6f1208de7a06b650e0e502dec1a042188c9f875b7"
+            "cb417e8a6ed65fc3596aeb2f6fb24d7651899f53dc757f03ea4be9311e00fbd7fecdbffeb3cab327864f34").decode("hex")
+        mobj, r = self.buildMsgObj(data, 1)
+
+        adCmd, (adRefs, adRefKey) = r[0]
+        self.assertEqual(adCmd, "advertIdRefs")
+        self.assertEqual(len(adRefs), 5)
+        self.assertEqual(adRefs, self.advertIdRefs[:5])
+        self.assertEqual(adRefKey, 'five')
+
+
+    def testDecode16Ref(self):
+        data = ("02353637387a6a431b669f1914f6313bc405905e134f098f6bcd4621d373cade4e832627b4f6b6f1208de7a06b650e0e502dec1a042188c9f875b7cb417e8a6e"
+            "d65fc3596aeb2f6fb24d7651899f53dc757f03ea4be9311e00fbd7fecdbffeb3cab327864f34812d363384d626e8c3610d46541eb0f6c58205ce914f74f9a968"
+            "f24fbd332cd27bf7250cd9586e095d2f03f044b4e8b6b06f8815af69d60f7d919d5ed17ece6158920777bc04ad5f4b9841503162af099b1eabbb244690821c58"
+            "fd77fd0470c455a557ae0ca4848abdc406f5b927da2e27dc9bc1544df3a03087a2a7776d0268454116acd8ee9e968f643fe28f24f32bb2a6c44eba3dbc99f4fb"
+            "386c60b996c00197961ffce1c3b7a9ef7e50f447e27d").decode("hex")
+        mobj, r = self.buildMsgObj(data, 1)
+
+        adCmd, (adRefs, adRefKey) = r[0]
+        self.assertEqual(adCmd, "advertIdRefs")
+        self.assertEqual(len(adRefs), 16)
+        self.assertEqual(adRefs, self.advertIdRefs[:16])
+        self.assertEqual(adRefKey, None)
+
+
+    def testDecode16RefKey(self):
+        data = ("02353637387a6a431b669f1914f6313bc405905e135f077369787465656e098f6bcd4621d373cade4e832627b4f6b6f1208de7a06b650e0e502dec1a042188c9"
+            "f875b7cb417e8a6ed65fc3596aeb2f6fb24d7651899f53dc757f03ea4be9311e00fbd7fecdbffeb3cab327864f34812d363384d626e8c3610d46541eb0f6c582"
+            "05ce914f74f9a968f24fbd332cd27bf7250cd9586e095d2f03f044b4e8b6b06f8815af69d60f7d919d5ed17ece6158920777bc04ad5f4b9841503162af099b1e"
+            "abbb244690821c58fd77fd0470c455a557ae0ca4848abdc406f5b927da2e27dc9bc1544df3a03087a2a7776d0268454116acd8ee9e968f643fe28f24f32bb2a6"
+            "c44eba3dbc99f4fb386c60b996c00197961ffce1c3b7a9ef7e50f447e27d").decode("hex")
+        mobj, r = self.buildMsgObj(data, 1)
+
+        adCmd, (adRefs, adRefKey) = r[0]
+        self.assertEqual(adCmd, "advertIdRefs")
+        self.assertEqual(len(adRefs), 16)
+        self.assertEqual(adRefs, self.advertIdRefs[:16])
+        self.assertEqual(adRefKey, 'sixteen')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Unittest Main  
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if __name__=='__main__':
+    tl = unittest.defaultTestLoader
+    ts = unittest.TestSuite()
+    ts.addTest(tl.loadTestsFromTestCase(TestAdvertRefDecode))
+
+    tr = ts.run(unittest.TestResult())
+    if not tr.wasSuccessful():
+        ts.debug()
+    else: 
+        print tr
+
---
+++ msgRouter/packet_v02/test/testMsgDecode.py
@@ -0,0 +1,111 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+##~ Copyright (C) 2002-2009  TechGame Networks, LLC.              ##
+##~                                                               ##
+##~ This library is free software; you can redistribute it        ##
+##~ and/or modify it under the terms of the BSD style License as  ##
+##~ found in the LICENSE file included with this distribution.    ##
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Imports 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import unittest
+
+from TG.blathernet.adverts import advertIdForNS
+from TG.blathernet.msgRouter import packet_v02 as packet
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Definitions 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class TestMsgDecode(unittest.TestCase):
+    advertId = advertIdForNS('testOne')
+    msgId = '1234'
+
+    def buildMsgObj(self, data, nCmds):
+        mobj = packet.MsgObject(data)
+        r = list(mobj.cmdList)
+        self.assertEqual(len(r),  nCmds)
+        return mobj, r
+
+    def testDecodeEmpty(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a311").decode("hex")
+        mobj, r = self.buildMsgObj(data, 0)
+
+    def testDecodeMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a31180000f612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0x0, None)))
+
+    def testDecodeMsgFmt(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a3118e000f612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0xe, None)))
+
+    def testDecode_3BTopicMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a3119e000f03646566612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0xe, 'def')))
+
+    def testDecode_4BTopicMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a311de000f61626364612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0xe, 'abcd')))
+
+    def testDecode_5BTopicMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a3119e000f056465666768612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0xe, 'defgh')))
+
+    def testDecode_8BTopicMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a311ee000f6162636465666768612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0xe, 'abcdefgh')))
+
+    def testDecode_13BTopicMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a3119e000f0d6162636465666768696a6b6c6d612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0xe, 'abcdefghijklm')))
+
+    def testDecode_16BTopicMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a311fe000fb64b56ec75c4c8d6ab16b894d3c0a311612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0xe, self.advertId)))
+
+    def testDecodeIntTopicMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a311ce000f42abcdef612073686f7274206d657373616765").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 1)
+        self.assertEqual(r[0], ('msg', ('a short message', 0xe, 0x42abcdef)))
+
+    def testDecodeTwoMsg(self):
+        data = ("0231323334b64b56ec75c4c8d6ab16b894d3c0a31180001574686973206973206d65737361676520616c70686180000462657461").decode("hex")
+
+        mobj, r = self.buildMsgObj(data, 2)
+        self.assertEqual(r[0], ('msg', ('this is message alpha', 0x0, None)))
+        self.assertEqual(r[1], ('msg', ('beta', 0x0, None)))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Unittest Main  
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if __name__=='__main__':
+    tl = unittest.defaultTestLoader
+    ts = unittest.TestSuite()
+    ts.addTest(tl.loadTestsFromTestCase(TestMsgDecode))
+
+    tr = ts.run(unittest.TestResult())
+    if not tr.wasSuccessful():
+        ts.debug()
+    else: 
+        print tr
+
